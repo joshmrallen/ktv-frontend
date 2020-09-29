64755bcfe3865ca85fab45d05d3b9262f7522e4e
github username <joshmrallen@gmail.com>
2020-09-29 18:13:12 -0400

    added signup component; TODO: add login, save jwt to state for persisted login

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from 'react-router-dom'
-import { ZoomMtg } from ‘@zoomus/websdk‘;
+// import { ZoomMtg } from ‘@zoomus/websdk‘;
 
 
 ReactDOM.render(
