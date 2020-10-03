aee999c1de97f546d601fd159568c147106b28d2
github username <joshmrallen@gmail.com>
2020-10-03 01:14:03 -0400

    commented out unused imports

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
-import ResultsContainer from './containers/ResultsContainer'
+// import ResultsContainer from './containers/ResultsContainer'
 import NavBar from './components/NavBar';
-import {Route, Switch, withRouter, NavLink} from 'react-router-dom'
+import {Route, Switch, withRouter } from 'react-router-dom'
 import Room from './containers/Room'
-import FavoritesContainer from './containers/FavoritesContainer'
+// import FavoritesContainer from './containers/FavoritesContainer'
 import Home from './components/Home'
 import SignUp from './components/SignUp'
 import Login from './components/Login'
