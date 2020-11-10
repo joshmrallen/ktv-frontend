b076df069cc8757213a3e26602259c661553e487
github username <joshmrallen@gmail.com>
2020-11-10 15:27:25 -0500

    navbar now hidden on landing page - see google doc for todo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,11 +4,11 @@ import { NavLink } from 'react-router-dom'
 const NavBar = (props) => {
     // console.log(props)
     return(
-        <div className='nav' style={{visibility: 'hidden'}}>
-            {props.user ? <NavLink className="link" activeClassName="activeLink" exact={true} to="/">Home</NavLink> : null}
-            {props.user ? <NavLink className="link" activeClassName="activeLink" to="/room">Room</NavLink> : null}
-            {props.user ? <span className='hellouser'>Welcome {props.user.name}!</span> : <NavLink className="link" activeClassName="activeLink" to="/login">Login</NavLink>}
-            {props.user ? <NavLink className="link" activeClassName="activeLink" exact to="/" onClick={props.logout}>Logout</NavLink> : <NavLink className="link" activeClassName="activeLink" to="/signup">Signup</NavLink>}
+        <div className='nav' style={props.user ? {visibility: 'visible'} : {visibility: 'hidden'}} >
+            <NavLink className="link" activeClassName="activeLink" exact={true} to="/">Home</NavLink>
+            <NavLink className="link" activeClassName="activeLink" to="/room">Room</NavLink>
+            <span className='hellouser'>Welcome {props.user.name}!</span> : <NavLink className="link" activeClassName="activeLink" to="/login">Login</NavLink>
+            <NavLink className="link" activeClassName="activeLink" exact to="/" onClick={props.logout}>Logout</NavLink> : <NavLink className="link" activeClassName="activeLink" to="/signup">Signup</NavLink>
         </div>
     )
 }
