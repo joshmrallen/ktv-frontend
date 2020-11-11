0be824661df4a847f62a8f55f299dc66da646975
redazures <jwu_nyc@yahoo.com>
2020-11-11 15:25:05 -0500

    css changes

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,8 +3,8 @@ import React from 'react'
 const Login = (props) => {
 
     return(
-        <div className='loadPage'>
-            <form onSubmit={props.submitHandler}>
+        <div className='home'>
+            <form className='form' onSubmit={props.submitHandler}>
                 <input type='email' className='email' name='email' placeholder='Email' onChange={props.changeHandler} value={props.email} />
                 <input type='password' className='password' name='password' placeholder='Password' onChange={props.changeHandler} value={props.password} />
                 <input type='submit' className='submit' value='Login!' />
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -3,8 +3,8 @@ import React from 'react'
 const SignUp = (props) => {
 
     return(
-        <div className='loadPage'>
-            <form className='signuppage' onSubmit={props.submitHandler}>
+        <div className='home'>
+            <form className='form' onSubmit={props.submitHandler}>
                 <input type='text' className='name' name='name' placeholder='Name' onChange={props.changeHandler} value={props.name} />
                 <input type='email' className='email' name='email' placeholder='Email' onChange={props.changeHandler} value={props.email} />
                 <input type='password' className='password' name='password' placeholder='Password' onChange={props.changeHandler} value={props.password} />
