6a67ecc2efeaa63841bb545511c699a5b272d933
github username <joshmrallen@gmail.com>
2020-11-11 16:12:33 -0500

    fixed login/signup form css and started mobile only css

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,11 +4,14 @@ const Login = (props) => {
 
     return(
         <div className='home'>
-            <form className='form' onSubmit={props.submitHandler}>
-                <input type='email' className='email' name='email' placeholder='Email' onChange={props.changeHandler} value={props.email} />
-                <input type='password' className='password' name='password' placeholder='Password' onChange={props.changeHandler} value={props.password} />
-                <input type='submit' className='submit' value='Login!' />
-            </form>
+            <div className='form-container'>
+                <h2>Login & Sing!</h2>
+                <form className='form' onSubmit={props.submitHandler}>
+                    <input type='email' className='email' name='email' placeholder='Email' onChange={props.changeHandler} value={props.email} />
+                    <input type='password' className='password' name='password' placeholder='Password' onChange={props.changeHandler} value={props.password} />
+                    <input type='submit' className='submit' value='Login!' />
+                </form>
+            </div>
         </div>
     )
 }
